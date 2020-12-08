56f58ff7a36ec0f52667e0177b02739bea4e9f03
o <omkkqq@gmail.com>
2020-12-08 17:46:31 +0800

    "whyyyyyyyyyyyyyyy"

--- app/src/main/java/com/example/xxxxxx/MainActivity.java
+++ app/src/main/java/com/example/xxxxxx/MainActivity.java
@@ -26,13 +26,19 @@ import com.github.rubensousa.previewseekbar.PreviewBar;
 import com.github.rubensousa.previewseekbar.PreviewLoader;
 import com.github.rubensousa.previewseekbar.PreviewSeekBar;
 import com.github.rubensousa.previewseekbar.exoplayer.PreviewTimeBar;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -45,22 +51,24 @@ public class MainActivity extends AppCompatActivity {
     private PreviewTimeBar previewTimeBar;
     private PreviewSeekBar previewSeekBar;
     private static double nowit=0;
+    private   ImageButton exo_next,exo_prev;
+    int current=0;
     private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        video_player_view=findViewById(R.id.video_player_view);
-        ImageButton button=video_player_view.findViewById(R.id.exo_ffwd);
-        imageView=findViewById(R.id.a15);
+        video_player_view = findViewById(R.id.video_player_view);
+        ImageButton button = video_player_view.findViewById(R.id.exo_ffwd);
+        imageView = findViewById(R.id.a15);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("rabbit", "onClick: "+getVideoDurationSeconds(videoPlayer)+"qqq   "+(videoPlayer.getCurrentPosition()+15000)/1000);
-                if (getVideoDurationSeconds(videoPlayer)<(videoPlayer.getCurrentPosition()+15000)/1000){
+                Log.d("rabbit", "onClick: " + getVideoDurationSeconds(videoPlayer) + "qqq   " + (videoPlayer.getCurrentPosition() + 15000) / 1000);
+                if (getVideoDurationSeconds(videoPlayer) < (videoPlayer.getCurrentPosition() + 15000) / 1000) {
 
-                    videoPlayer.seekTo(getVideoDurationSeconds(videoPlayer)*1000);
+                    videoPlayer.seekTo(getVideoDurationSeconds(videoPlayer) * 1000);
                     imageView.setVisibility(View.VISIBLE);
                     imageView.animate()
                             .translationY(view.getHeight())
@@ -72,8 +80,7 @@ public class MainActivity extends AppCompatActivity {
                                     imageView.setVisibility(View.GONE);
                                 }
                             });
-                }
-                else {
+                } else {
                     videoPlayer.seekTo(videoPlayer.getContentPosition() + 15000);
                     imageView.setVisibility(View.VISIBLE);
                     imageView.animate()
@@ -89,117 +96,51 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
-
-
                 //imageView.setVisibility(View.GONE);
             }
         });
-         initializePlayer();
-//        video_player_view = findViewById(R.id.player_view);
-//        previewTimeBar = video_player_view.findViewById(R.id.exo_progress);
-//        previewSeekBar = findViewById(R.id.previewSeekBar);
-//
-//        previewTimeBar.addOnPreviewVisibilityListener((previewBar, isPreviewShowing) -> {
-//            Log.d("PreviewShowing", String.valueOf(isPreviewShowing));
-//        });
-//
-//        previewTimeBar.addOnScrubListener(new PreviewBar.OnScrubListener() {
-//            @Override
-//            public void onScrubStart(PreviewBar previewBar) {
-//                Log.d("Scrub", "START");
-//            }
-//
-//            @Override
-//            public void onScrubMove(PreviewBar previewBar, int progress, boolean fromUser) {
-//
-//               nowit=(progress/1000);
-//                  Log.d("Scrub", "MOVE to " + nowit + " FROM USER: " + fromUser);
-//                try {
-//                    exoPlayerManager.setBitmap(retriveVideoFrameFromVideo(sampleUrl));
-//                } catch (Throwable throwable) {
-//                    throwable.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void onScrubStop(PreviewBar previewBar) {
-//                Log.d("Scrub", "STOP");
-//            }
-//        });
-//
-//        exoPlayerManager = new ExoPlayerManager(video_player_view, previewTimeBar,
-//                (ImageView) findViewById(R.id.imageView), getString(R.string.url_thumbnails));
-//
-//        exoPlayerManager.play(Uri.parse(sampleUrl));
-//
+
+         exo_next=video_player_view.findViewById(R.id.exo_next);
+        exo_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                exo_prev.setImageDrawable(getDrawable(R.drawable.exo_icon_previous));
+                next();
+                current++;
+
+                Log.d("cccccccccccc", "onClick: "+current);
+            }
+        });
+
+         exo_prev=video_player_view.findViewById(R.id.exo_prev);
+        exo_prev.setOnClickListener(view -> {
+
+            if (current<0){
+                exo_prev.setImageDrawable(getDrawable(R.drawable.ic_launcher_background));
+            }else {
+               previous();
+            }
+            current--;
+            if (current<=0){
+                current=0;
+                exo_prev.setImageDrawable(getDrawable(R.drawable.ic_launcher_background));
+
+            }
+                    Log.d("cccccccccccc", "onClick: "+current);
+
+        } );
+        initializePlayer();
+        int previousWindowIndex = videoPlayer.getPreviousWindowIndex();
+        if (previousWindowIndex != C.INDEX_UNSET) {
+//            previous();
+        } else {
+
+            exo_prev.setImageDrawable(getDrawable(R.drawable.ic_launcher_background));
+            Log.d("uuu", "previous: ");
+        }
 
     }
-//    @Override
-//    public void onWindowFocusChanged(boolean hasFocus) {
-//        super.onWindowFocusChanged(hasFocus);
-//        if (hasFocus) {
-//            requestFullScreenIfLandscape();
-//        }
-//    }
-//public static Bitmap retriveVideoFrameFromVideo(String videoPath) throws Throwable
-//{
-//    Bitmap bitmap = null;
-//    MediaMetadataRetriever mediaMetadataRetriever = null;
-//    try
-//    {
-//        mediaMetadataRetriever = new MediaMetadataRetriever();
-//        if (Build.VERSION.SDK_INT >= 14)
-//            mediaMetadataRetriever.setDataSource(videoPath, new HashMap<String, String>());
-//        else
-//            mediaMetadataRetriever.setDataSource(videoPath);
-//        //   mediaMetadataRetriever.setDataSource(videoPath);
-//
-//        bitmap = mediaMetadataRetriever.getFrameAtTime(Math.round(nowit) * 1000000,
-//                MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-//       // bitmap = mediaMetadataRetriever.getFrameAtTime();
-//    } catch (Exception e) {
-//        e.printStackTrace();
-//        throw new Throwable("Exception in retriveVideoFrameFromVideo(String videoPath)" + e.getMessage());
-//
-//    } finally {
-//        if (mediaMetadataRetriever != null) {
-//            mediaMetadataRetriever.release();
-//        }
-//    }
-//    return bitmap;
-//}
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        exoPlayerManager.onStart();
-//    }
-//
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        exoPlayerManager.onResume();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        exoPlayerManager.onPause();
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        exoPlayerManager.onStop();
-//    }
-
-//    TimerTask task= new TimerTask() {
-//        @Override
-//        public void run() {
-//
-//        }
-//    };
-//
-
     private int getVideoDurationSeconds(SimpleExoPlayer player)
     {
         double timeMs=(double) player.getDuration();
@@ -219,12 +160,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private MediaSource buildMediaSource() {
-        DefaultDataSourceFactory dataSourceFactory
-                =new DefaultDataSourceFactory(this,"sample");
-        return new  ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(Uri.parse(sampleUrl));
+//        DefaultDataSourceFactory dataSourceFactory
+//                =new DefaultDataSourceFactory(this,"sample");
+//        return new  ProgressiveMediaSource.Factory(dataSourceFactory)
+//                .createMediaSource(Uri.parse(sampleUrl));
+        return getMediaSource();
     }
 
+
+    private MediaSource getMediaSource() {
+        // 将URL组装成资源类
+        DefaultDataSourceFactory dataSourceFactory
+                =new DefaultDataSourceFactory(this,"sample");
+        List<String> urlList=new ArrayList<>();
+        urlList.add("http://iottalk.cmoremap.com.tw:6333/qbee205/remote.php/direct/RnyI5s24NiPEoetqYjagY8vDM4xUWfaH6dud9MB2uDX4Odyd52QKyPOBnMWA");
+        urlList.add("http://iottalk.cmoremap.com.tw:6333/qbee205/remote.php/direct/gPErJL3uEd9Pf8EwudiF2ObfL4c4Mh9hHd1rWd2xlqs0VnZwIoB2sT8oM5cj");
+        urlList.add("http://iottalk.cmoremap.com.tw:6333/qbee205/remote.php/direct/TO76m58n8qbhush0AuUUNuYUrJdsHiHJ7mHdFJ22245NeDQfaJHPPSixtstn");
+
+       urlList.add("http://iottalk.cmoremap.com.tw:6333/qbee205/remote.php/direct/J1tVnzLzyF8lNRm8k4x8Xq1OE0NJIgorxW564iIm5XUKGefaYkOc1a0q0lbR");
+        MediaSource[] mediaSources = new MediaSource[urlList.size()];
+        for (int i = 0; i < urlList.size(); i++) {
+            Uri uri = Uri.parse(urlList.get(i));
+
+            mediaSources[i] = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        }
+        MediaSource mediaSource = mediaSources.length == 1 ? mediaSources[0] : new ConcatenatingMediaSource(mediaSources);
+//        if (loop) {
+//            mediaSource = new LoopingMediaSource(mediaSource, loopCount);
+//        }
+        return mediaSource;
+    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -237,6 +202,40 @@ public class MainActivity extends AppCompatActivity {
         videoPlayer.setPlayWhenReady(false);
         if (((Activity)this).isFinishing()){
             releasePlayer();
+        }
+    }
+
+    /**
+     * 下一首
+     */
+    public void next() {
+        if (null != videoPlayer) {
+            if (videoPlayer.getCurrentTimeline().isEmpty()) {
+                return;
+            }
+            int nextWindowIndex = videoPlayer.getNextWindowIndex();
+            if (nextWindowIndex != C.INDEX_UNSET) {
+                videoPlayer.seekTo(nextWindowIndex, C.TIME_UNSET);
+            } else {
+                Log.d("info", "已经是最后一个了");
+            }
+        }
+    }
+
+    /**
+     * 上一首
+     */
+    public void previous() {
+        if (null != videoPlayer) {
+            if (videoPlayer.getCurrentTimeline().isEmpty()) {
+                return;
+            }
+            int previousWindowIndex = videoPlayer.getPreviousWindowIndex();
+            if (previousWindowIndex != C.INDEX_UNSET) {
+                videoPlayer.seekTo(previousWindowIndex, C.TIME_UNSET);
+            } else {
+                Log.d("tttttt", "previous: ");
+            }
         }
     }
 
